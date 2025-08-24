b21c0c75b3f9aa5ae67710bcb749511654749ff8
laizu01993 <laizuaktar6@gmail.com>
2025-08-25 02:37:47 +0600

    added banner image animation in the banner component using framer motion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,15 +1,26 @@
 import { easeOut, motion } from "motion/react"
+import banner1 from "../../assets/banner1.jpg"
+import banner2 from "../../assets/banner2.jpg"
+
 const Banner = () => {
     return (
         <div className="hero bg-green-50 min-h-96 mt-4">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="flex-1">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl"
+                    <motion.img
+                        src={banner1}
+                        animate={{y: [50, 100, 50]}}
+                        transition={{duration: 5, repeat: Infinity}}
+                        className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl w-80 border-b-4 border-l-4 border-green-500"
+                    />
+                    <motion.img
+                        src={banner2}
+                        animate={{x: [100, 150, 100]}}
+                        transition={{duration: 5, repeat: Infinity}}
+                        className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl w-80 border-b-4 border-l-4 border-green-500"
                     />
                 </div>
-                <div className="flex-1">
+                <div className="flex-1 overflow-hidden">
                     <motion.h1 className="text-4xl md:text-5xl font-extrabold text-gray-800"
                         animate={{ x: 50 }}
                         transition={{duration: 2, delay: 1, ease: easeOut, repeat: Infinity}}>
